dd2787b185826bf93b1f8b62e6a8c927a3f52298
Nathan Dyck <ndyck42@gmail.com>
2015-03-20 20:26:18 -0500

    pulled out into a separate package so it could be tested

--- api.pl
+++ api.pl
@@ -3,69 +3,8 @@ use local::lib;
 use FindBin;
 use lib "$FindBin::Bin/lib";
 use Mojolicious::Lite;
-use Mojo::JSON;
-use DBIx::Class;
-use TicTacToe;
-use TicTacToe::Schema; 
-
-# Global handle for schema connection
-my $schema = "";
-
-# Create Schema as needed
-helper schema => sub {
-    if ($schema) {
-        return $schema;
-    } else {
-        #build an in memory SQLite db from our DBIx Schema
-        $schema = TicTacToe::Schema->connect('dbi:SQLite::memory:');
-        $schema->deploy;
-        return $schema;
-    }
-};
-
-get '/' => sub {
-  my $c = shift;
-  $c->render(text => 'TicTacToe API Example');
-};
-
-get '/api/newgame' => sub {
-    my $self = shift;
-    
-    my $game = $self->schema->resultset('Game')->create({});
-    if ($game) {
-        return $self->render(json => { success => \1, game_id => $game->id});
-    } else {
-        return $self->render(json => { success => \0, message => 'Unable to create game'});
-    }
-};
-
-get '/api/move/:game_id/:move' => sub {
-    my $self = shift;
-    my $game_id  = $self->stash('game_id');
-    my $move  = $self->stash('move');
-    
-    my $game = $self->schema->resultset('Game')->find($game_id);
-    
-    my $success;
-    my $results;
-    if ($game) {
-        $results = $game->move($move);
-        if (
-            ($results eq 'No winner yet') ||
-            ($results =~ /^Player: [0..1] wins$/) ||
-            ($results eq 'Board is full with no winner. Cat\'s game.')
-        ) {
-            $success = \1;
-        } else {
-            $success = \0;
-        }
-        
-        return $self->render(json => { success => $success, message => $results, game_id => $game->id });
-    } else {
-        return $self->render(json => { success => \0, message => "Invalid game_id: $game_id" });
-    }
-};
-
+use TicTacToe::API; 
 
 # Start the app
-app->start;
+my $app = TicTacToe::API->new;
+$app->start;
---
+++ lib/TicTacToe/API.pm
@@ -0,0 +1,68 @@
+package TicTacToe::API;
+
+use local::lib;
+use FindBin;
+use lib "$FindBin::Bin/..";
+use Modern::Perl;
+use Mojo::Base 'Mojolicious';
+use Mojolicious::Lite;
+use TicTacToe::Schema;
+
+
+# Global handle for schema connection
+my $schema = "";
+
+# Create Schema as needed
+helper schema => sub {
+    if ($schema) {
+        return $schema;
+    } else {
+        #build an in memory SQLite db from our DBIx Schema
+        $schema = TicTacToe::Schema->connect('dbi:SQLite::memory:');
+        $schema->deploy;
+        return $schema;
+    }
+};
+
+get '/' => sub {
+  my $c = shift;
+  $c->render(text => 'TicTacToe API Example');
+};
+
+get '/api/newgame' => sub {
+    my $self = shift;
+    
+    my $game = $self->schema->resultset('Game')->create({});
+    if ($game) {
+        return $self->render(json => { success => \1, game_id => $game->id});
+    } else {
+        return $self->render(json => { success => \0, message => 'Unable to create game'});
+    }
+};
+
+get '/api/move/:game_id/:move' => sub {
+    my $self = shift;
+    my $game_id  = $self->stash('game_id');
+    my $move  = $self->stash('move');
+    
+    my $game = $self->schema->resultset('Game')->find($game_id);
+    
+    my $success;
+    my $results;
+    if ($game) {
+        $results = $game->move($move);
+        if (
+            ($results eq 'No winner yet') ||
+            ($results =~ /^Player: [0..1] wins$/) ||
+            ($results eq 'Board is full with no winner. Cat\'s game.')
+        ) {
+            $success = \1;
+        } else {
+            $success = \0;
+        }
+        
+        return $self->render(json => { success => $success, message => $results, game_id => $game->id });
+    } else {
+        return $self->render(json => { success => \0, message => "Invalid game_id: $game_id" });
+    }
+};
